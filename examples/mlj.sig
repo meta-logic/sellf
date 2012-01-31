0cc94bb72a739750af40d80a4f5dd588cd888475
Giselle Reis <giselle.mnr@gmail.com>
2012-01-31 11:05:40 +0000

    Organizing examples.
    git-svn-id: https://sellf.googlecode.com/svn/trunk@213 9dc659ae-9c01-8992-996e-dd6216b5bf81

--- examples/mlj.sig
+++
@@ -1,9 +0,0 @@
-kind form type.
-
-type and form -> form -> form.
-type imp form -> form -> form.
-
-type lft form -> o.
-type rght form -> o.
-
-type solve o.
---
+++ examples/proofsystems/lax.pl
@@ -0,0 +1,42 @@
+subexp defs unb.
+subexp l unb.
+subexp r unb.
+subexp rr unb.
+
+subexprel l <= defs.
+subexprel r <= defs.
+subexprel rr <= defs.
+subexprel r <= rr.
+subexprel rr <= l.
+
+context defs.
+
+% Not possible because there's no way to put the cut rule in the context.
+
+%lft (bottom).
+
+% Init and cut rules are not part of the specification
+
+%lft (A) :: rght (A).
+% I think this question mark is not allowed on the left-hand side. Returns
+% syntax error.
+%[l]? lft (A) :: [r]? rght (A).
+
+%% In Elaine's specification, & is substituted with oplus (;)
+
+lft (imp A B) :: ([r]? (rght A)) & ([l]? (lft B)).
+rght (imp A B) :: [l]bang ( ([l]? (lft A)) | ([r]? (rght B))).
+
+lft (and A B) :: ([l]? (lft A)) | ([l]? (lft B)).
+rght (and A B) :: ([r]? (rght A)) & ([r]? (rght B)).
+
+lft (or A B) :: ([l]? (lft A)) & ([l]? (lft B)).
+rght (or A B) :: ([r]? (rght A)) | ([r]? (rght B)).
+
+% Think about how to define quantifiers.
+%lft (forall A) :: [l]? (pi X\ (lft (A X))).
+%rght (forall A) :: [l]bang (pi x\ ([r]? (rght A x))).
+
+%lft (exists A) :: pi x\ ([l]? (lft A x)).
+%rght (exists A) :: [r]? (rght B X).
+
