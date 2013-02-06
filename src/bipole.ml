93ee9b9efcbc657c0c6d29abf5d75414564761eb
Giselle Reis <giselle.mnr@gmail.com>
2013-02-06 17:27:53 +0000

    Not much...
    git-svn-id: https://sellf.googlecode.com/svn/trunk@305 9dc659ae-9c01-8992-996e-dd6216b5bf81

--- src/bipole.ml
+++ src/bipole.ml
@@ -26,7 +26,7 @@ let deriveBipole seq form constr =
 
   (* Builds the derivation of f as a bipole (one positive and one negative
   phase). 'acc' holds the resulting pairs (prooftree * constraints list)?? *)
-  let derive (prooftree, constraintlst) acc(?) = 
+  let rec derive (prooftree, constraintlst) suc = 
     let conclusion = ProofTreeSchema.getConclusion prooftree in
     match (SequentSchema.getPhase conclusion, SequentSchema.getGoals) with
 
@@ -36,14 +36,56 @@ let deriveBipole seq form constr =
       | PARR(_,_)
       | TOP
       | BOT
-      | FORALL(_,_)
+      | FORALL(_,_,_)
       | QST(_) ->
-        let (pt, c) = ProofTreeSchema.releaseDown pt
-        derive (pt, constraintlst.map(fun cst -> Constraint.add cst c))
+        let (pt, c) = ProofTreeSchema.releaseDown pt in
+        derive (pt, constraintlst.map(fun cst -> Constraint.add cst c)) suc
 
-    | ASYN, hd::tl -> 
+      | ADDOR(f1, f2) ->
+
+      | TENSOR(f1, f2) ->
+
+      | EXISTS(s, i, f1) ->
+
+      | ONE ->
+
+      | BANG(f1) ->
+
+      | PRED(str, terms, POS) ->
+
+      | NOT(PRED(str, terms, NEG)) ->
+
+    | ASYN, hd::tl -> match Term.observe hd with
+      (* Release rule *)
+      | ADDOR(_,_) 
+      | TENSOR(_,_)
+      | EXISTS(_,_,_)
+      | ONE
+      | BANG(_)
+      | PRED(_,_,POS)
+      | NOT(PRED(_,_,NEG)) ->
+        (* TODO: implement releaseUp rule *)
+        let (pt, c) = ProofTreeSchema.releaseUp pt hd in
+        derive (pt, constraintlst.map(fun cst -> Constraint.add cst c)) suc
+
+      | WITH(f1, f2) ->
+
+      | PARR(f1, f2) ->
+
+      | TOP ->
+
+      | BOT ->
+      
+      | FORALL(s, i, f1) ->
+
+      | QST(f1) ->
+
+    (* Do not decide for a second time. The end of this phase means the end of
+    the bipole. *)
+    | ASYN, [] -> suc
 
 
   in
-  derive (pt1, [(decidecstr @ constr)]) []
+  derive (pt1, [(decidecstr @ constr)]) (fun -> (* TODO implement success
+  function *))
 
