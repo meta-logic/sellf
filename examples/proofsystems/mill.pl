ca77c0b97a519886590002d40e245e68ea65b8ed
Giselle Reis <giselle.mnr@gmail.com>
2016-02-21 16:26:33 +0100

    Adding specifications for MILL
    Multiplicative Intuitionistic Linear Logic

---
+++ examples/proofsystems/mill.pl
@@ -0,0 +1,37 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                               %
+% SELLF specification for MILL                  %
+% (Multiplicative intuitionistic linear logic)  %
+%                                               %
+% Giselle Reis  -   2016                        %
+%                                               %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+subexp l lin.
+subexp r lin.
+
+subexpctx l many ant.
+subexpctx r single suc.
+
+rules introduction.
+
+% Implication
+%% [l]bang must require r to be empty.
+(not (lft (lolli A B))) * (([l]bang ([r]? (rght A))) * ([l]? (lft B))).
+(not (rght (lolli A B))) *  (([l]? (lft A)) | ([r]? (rght B))).
+
+% Tensor
+(not (lft (tensor A B))) * (([l]? (lft A)) | ([l]? (lft B))).
+(not (rght (tensor A B))) * (([r]? (rght A)) * ([r]? (rght B))).
+
+% One
+(not (lft lone)) * bot.
+(not (rght lone)) * one.
+
+rules axiom.
+((not (lft A)) * (not (rght A))).
+
+rules cut.
+(([r]? (rght A)) * ([l]? (lft A))).
+
+
---
+++ examples/proofsystems/mill.sig
@@ -0,0 +1,6 @@
+% Object logic connectives' types
+
+type lolli form -> form -> form.
+type tensor  form -> form -> form.
+type lone form.
+
