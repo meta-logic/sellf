84029e6b2871392acaf5cfd4682907bcf81e5f7d
Jize Ning <jizen@andrew.cmu.edu>
2018-06-01 18:17:28 +0800

    complete cc and ic

--- src/test_cc.ml
+++ src/test_cc.ml
@@ -58,3 +58,44 @@ let test5_cc test_ctxt =
         assert_equal true (check_cutcoherence specName);;
 
 
+let test6_cc test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/ill";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_cutcoherence specName);;
+
+let test7_cc test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/mlj";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_cutcoherence specName);;
+
+let test8_cc test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/s4";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_cutcoherence specName);;
+
+let test9_cc test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/g3k";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_cutcoherence specName);;
+
+
+let test10_cc test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/lax";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_cutcoherence specName);;
+
