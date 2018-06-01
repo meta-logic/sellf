3cf2136b05838632e364011430fbc95970c8c5a9
Jize Ning <jizen@andrew.cmu.edu>
2018-06-01 18:17:28 +0800

    complete cc and ic

--- src/test_ic.ml
+++ src/test_ic.ml
@@ -26,3 +26,68 @@ let test1_ic test_ctxt =
       let specName = Str.string_after !fileName (idx+1) in 
         assert_equal 1 (check_initialcoherence specName);;
 
+let test2_ic test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/lal";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_initialcoherence specName);;
+
+let test3_ic test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/lj";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_initialcoherence specName);;
+
+let test4_ic test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/lk";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_initialcoherence specName);;
+
+let test5_ic test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/ll";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_initialcoherence specName);;
+
+
+let test6_ic test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/ill";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_initialcoherence specName);;
+
+let test7_ic test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/mlj";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_initialcoherence specName);;
+
+let test8_ic test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/s4";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_initialcoherence specName);;
+
+let test9_ic test_ctxt = 
+  initAll();
+  let fileName = ref "../examples/proofsystems/g3k";
+       in
+    let idx = String.rindex !fileName '/' in
+      let specName = Str.string_after !fileName (idx+1) in 
+        assert_equal 1 (check_initialcoherence specName);;
+
--- src/test_main.ml
+++ src/test_main.ml
@@ -18,10 +18,22 @@ let suite =
  "test3_cc">:: Test_cc.test3_cc;
  "test4_cc">:: Test_cc.test4_cc;
  "test5_cc">:: Test_cc.test5_cc;
+ "test6_cc">:: Test_cc.test6_cc;
+ "test7_cc">:: Test_cc.test7_cc;
+ "test8_cc">:: Test_cc.test8_cc;
+ "test9_cc">:: Test_cc.test9_cc;
 
 
  (* Testing initial coherence *)
-  "test1_ic">:: Test_ic.test1_ic
+  "test1_ic">:: Test_ic.test1_ic;
+  "test2_ic">:: Test_ic.test2_ic;
+  "test3_ic">:: Test_ic.test3_ic;
+  "test4_ic">:: Test_ic.test4_ic;
+  "test5_ic">:: Test_ic.test5_ic;
+  "test6_ic">:: Test_ic.test6_ic;
+  "test7_ic">:: Test_ic.test7_ic;
+  "test8_ic">:: Test_ic.test8_ic;
+  "test9_ic">:: Test_ic.test9_ic
 
   ]
 ;;
